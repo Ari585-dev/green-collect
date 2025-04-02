14b7282201f7dd4928b43c614df6913070450c13
Ari585-dev <arigames585@gmail.com>
2025-04-01 21:07:21 -0500

    Corrección petición http, de GET a POST

--- src/modules/notifications/notifications.service.ts
+++ src/modules/notifications/notifications.service.ts
@@ -8,12 +8,13 @@ export class NotificationsService {
         const authToken = process.env.TWILIO_AUTH_TOKEN;
         const twilioNumber = process.env.TWILIONUMBER; // Asegúrate de que sea un número válido de Twilio
         const client = Twilio(accountSid, authToken);
+        const userPhone= process.env.TWILIO_PHONE_NUMBER;
 
         client.messages
             .create({
                 body: 'Mensaje de prueba desde NestJS',
                 from: `whatsapp:+14${twilioNumber}`,
-                to: 'whatsapp:+573107511845',
+                to: `whatsapp:${userPhone}`,
             })
             .then(message => console.log('Mensaje enviado:', message.sid))
             .catch(error => console.error('Error enviando mensaje:', error));
