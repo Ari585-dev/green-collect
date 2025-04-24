1ad4212b7b3fd45818fdbd6f1cf64e86e2f64cdd
Ari585-dev <arigames585@gmail.com>
2025-04-24 11:10:01 -0500

    Frontend para el registro y login

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { NotificationsController } from './modules/notifications/notifications.controller';
 import { NotificationsModule } from './modules/notifications/notifications.module';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './modules/auth/auth.module';
-
+import { FrontendModule } from './frontend/frontend.module';
 
 @Module({
-  imports: [NotificationsModule, DatabaseModule, AuthModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+      exclude: ['/api*'],
+    }),
+    NotificationsModule,
+    DatabaseModule,
+    AuthModule,
+    FrontendModule
+  ],
   controllers: [NotificationsController],
   providers: [],
 })
---
+++ src/frontend/frontend.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get, Res } from '@nestjs/common';
+import { Response } from 'express';
+import { join } from 'path';
+
+@Controller()
+export class FrontendController {
+  @Get('/')
+  getRoot(@Res() res: Response) {
+    return res.sendFile(join(__dirname, '..', '..', 'public', 'index.html'));
+  }
+
+  @Get('/login')
+  getLogin(@Res() res: Response) {
+    return res.sendFile(join(__dirname, '..', '..', 'public', 'index.html'));
+  }
+
+  @Get('/register')
+  getRegister(@Res() res: Response) {
+    return res.sendFile(join(__dirname, '..', '..', 'public', 'index.html'));
+  }
+}
