bf211ea59aa720a79c0de971d4d981cc11cc4c5f
Ari585-dev <arigames585@gmail.com>
2025-04-28 12:03:12 -0500

    Confirmación por wh cuando se registra la request de recolección

--- src/modules/requests/requests.module.ts
+++ src/modules/requests/requests.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { RequestsController } from './requests.controller';
 import { RequestsService } from './requests.service';
+import { NotificationsService } from '../notifications/notifications.service';
 @Module({
       controllers: [RequestsController],
-      providers: [RequestsService]
+      providers: [RequestsService, NotificationsService]
 })
 export class RequestsModule {}
--- src/modules/requests/requests.service.ts
+++ src/modules/requests/requests.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import connection from 'src/config/database.connection';
+import { NotificationsService } from '../notifications/notifications.service'; // importa NotificationsService
+import { connection } from '../../config/database.connection'; // tu conexión MySQL
 
 @Injectable()
 export class RequestsService {
+  constructor(private notificationsService: NotificationsService) {}
+
   async createRequest(data: any) {
     const { user_id, residue_type, company_name, scheduled_date, weight_kg } = data;
 
@@ -10,9 +13,9 @@ export class RequestsService {
       throw new Error('Todos los campos son requeridos');
     }
 
-
     const formattedScheduledDate = new Date(scheduled_date).toISOString().slice(0, 19).replace('T', ' ');
 
+    // 1. Insertamos la solicitud
     await connection.promise().query(
       `INSERT INTO collection_requests 
         (user_id, scheduled_date, weight_kg, residue_type, company_name) 
@@ -20,7 +23,21 @@ export class RequestsService {
       [user_id, formattedScheduledDate, weight_kg, residue_type, company_name]
     );
 
-    return { message: 'Solicitud de recolección creada exitosamente' };
+    // 2. Buscamos el número de teléfono del usuario
+    const [rows]: any = await connection.promise().query(
+      'SELECT phone_number FROM users WHERE id = ?;',
+      [user_id]
+    );
+
+    if (rows.length === 0) {
+      throw new Error('Usuario no encontrado');
+    }
+
+    const phone = rows[0].phone_number;
+    const msg= `Se ha recibido tu petición de recolección para el día ${formattedScheduledDate}, recoger residuo: ${residue_type}, peso: ${weight_kg}kg`
+    
+    await this.notificationsService.sendWhatssappMessage({ phone, msg });
+    return { message: 'Solicitud de recolección creada exitosamente y mensaje enviado' };
   }
 
   async getMyRequests(userId: number) {
